1dbfe456124be17f0fc47f36c42a7ffc7cee4476
FMotalleb <fmotalleb@gmail.com>
2022-06-15 18:27:18 +0430

    feat (builder): change name of the apk file

--- lib/features/build_tools/core/build_toolkit.dart
+++ lib/features/build_tools/core/build_toolkit.dart
@@ -2,10 +2,27 @@ import 'dart:io';
 
 import 'package:hemend_toolkit/core/dependency_injector/basic_dependency_injector.dart';
 import 'package:hemend_toolkit/core/io/command_line_toolkit/command_line_tools.dart';
+import 'package:yaml/yaml.dart';
 
 import 'contracts/build_config/build_config.dart';
 
 abstract class BuildToolkit {
+  static const kDefaultApkOutput = 'build/app/outputs/flutter-apk/app-release.apk';
+  static String _buildAppName(String suffix) {
+    final config = loadYaml(File('pubspec.yaml').readAsStringSync()) as YamlMap;
+    final buffer = StringBuffer();
+    buffer.write(config['name']);
+    buffer.write('-v.');
+    buffer.write(config['version']);
+    buffer.write('-');
+    buffer.write(suffix);
+    buffer.write('-');
+    final dt = DeInjector.get<DateTime>().toIso8601String();
+    buffer.write(dt.substring(0, dt.lastIndexOf('.')));
+
+    return buffer.toString();
+  }
+
   static Future<void> _buildCommand(IBuildConfig buildConfig) async {
     final params = await buildConfig.builderParams;
 
@@ -19,8 +36,11 @@ abstract class BuildToolkit {
       exit(runResult.exitCode);
     } else {
       HemTerminal.I.printToConsole('Build Done:\n${runResult.stdout}\n${runResult.stderr}');
-      //TODO handle apk file name
 
+      final finalApk = File(kDefaultApkOutput);
+      final String appName = _buildAppName(buildConfig.buildType.name);
+      finalApk.renameSync('outputs/$appName.apk');
+      HemTerminal.I.printToConsole('Build output: outputs/$appName.apk');
     }
   }
 
