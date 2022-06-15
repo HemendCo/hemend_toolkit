296549df6debf4dfa383f871e1d8a8c27938511b
FMotalleb <fmotalleb@gmail.com>
2022-06-15 13:00:20 +0430

    feat (Git Tools): last commit authors email

--- lib/features/git_toolkit/git_toolkit.dart
+++ lib/features/git_toolkit/git_toolkit.dart
@@ -1,16 +1,30 @@
 import 'package:hemend_toolkit/core/io/command_line_toolkit/command_line_tools.dart';
 
 abstract class GitToolkit {
-  static const List<String> _getLastCommitHash = [
-    'log',
-    '-n 1',
-    '--pretty=format:"%H"',
-  ];
+  static List<String> get _getLastCommitHash => [
+        'log',
+        '-n 1',
+        '--pretty=format:"%H"',
+      ];
+  static List<String> get _getLastCommitAuthorEmail => [
+        'log',
+        '-n 1',
+        '--pretty=format:"%ae"',
+      ];
   static Future<String> getLastCommitsHash() async {
     final result = await HemTerminal.I.runTaskInTerminal(
       name: "Getting last commit's hash",
       command: 'git',
       arguments: _getLastCommitHash,
+    );
+    return result.stdout.toString().replaceAll('"', '');
+  }
+
+  static Future<String> getLastCommitsAuthorEmail() async {
+    final result = await HemTerminal.I.runTaskInTerminal(
+      name: "Getting last commit's author's email",
+      command: 'git',
+      arguments: _getLastCommitAuthorEmail,
     );
     return result.stdout.toString().replaceAll('"', '');
   }
