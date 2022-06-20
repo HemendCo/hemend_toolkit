1bd6157fa3812d22bf2f4aa6e77e61c34c9a7cd6
FMotalleb <fmotalleb@gmail.com>
2022-06-20 10:01:05 +0430

    [deinjector] possibility to set instance name

--- lib/core/dependency_injector/basic_dependency_injector.dart
+++ lib/core/dependency_injector/basic_dependency_injector.dart
@@ -1,18 +1,37 @@
+// typedef KeyValuePair = MapEntry<String, Type>;
+
 abstract class DeInjector {
-  static final Map<Type, dynamic> _dependencies = {};
-  static void register<T>(T dependency) {
-    _dependencies[T] = dependency;
+  static final Map<KeyValuePair, dynamic> _dependencies = {};
+  static void register<T>(T dependency, [String instanceName = 'base']) {
+    _dependencies[KeyValuePair(instanceName, T)] = dependency;
   }
 
-  static T get<T>() {
-    final dep = _dependencies[T];
+  static Map<KeyValuePair, dynamic> get getAll => _dependencies;
+  static T get<T>([String instanceName = 'base']) {
+    final dep = _dependencies[KeyValuePair(instanceName, T)];
     if (dep is Function) {
       return dep();
     }
     return dep;
   }
 
-  static void registerFactory<T>(T Function() factory) {
-    _dependencies[T] = factory;
+  static void registerFactory<T>(T Function() factory, [String instanceName = 'base']) {
+    _dependencies[KeyValuePair(instanceName, T)] = factory;
   }
+}
+
+class KeyValuePair {
+  final String base;
+  final Type pair;
+  const KeyValuePair(this.base, this.pair);
+  @override
+  bool operator ==(Object? other) {
+    if (other is KeyValuePair) {
+      return base == other.base && pair == other.pair;
+    }
+    return false;
+  }
+
+  @override
+  int get hashCode => base.hashCode ^ pair.hashCode;
 }
