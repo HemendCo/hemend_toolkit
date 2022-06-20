eeb2c8ab94f0fd45296f7d2e5336c97dbbb6259b
FMotalleb <fmotalleb@gmail.com>
2022-06-20 10:29:30 +0430

    changed behavior of dependency injection

--- lib/core/dependency_injector/basic_dependency_injector.dart
+++ lib/core/dependency_injector/basic_dependency_injector.dart
@@ -1,27 +1,36 @@
-/// create an instance of [_DeInjector]
-final deInjector = _DeInjector();
+/// create an instance of [_DeInjector] at runtime
+/// to avoid static initialization
+final deInjector = _DeInjector({});
 
 /// basic dependency injector
 class _DeInjector {
   /// instance of dependencies with a (key-value pair) as key
   /// that holds the dependency type and the dependency instance name
   /// and point them to the instance of the dependency
-  final Map<_InjectorKey, dynamic> _dependencies = {};
-  _DeInjector();
+  final Map<_InjectorKey, dynamic> _dependencies;
+  const _DeInjector(this._dependencies);
   static const _baseInstanceName = '';
 
   /// register a singleton dependency
-  void register<T>(T dependency, [String instanceName = _baseInstanceName]) {
+  void register<T>(
+    T dependency, [
+    String instanceName = _baseInstanceName,
+  ]) {
     _dependencies[_InjectorKey(instanceName, T)] = dependency;
   }
 
   /// register a factory for a dependency
-  void registerFactory<T>(T Function() factory, [String instanceName = _baseInstanceName]) {
+  void registerFactory<T>(
+    T Function() factory, [
+    String instanceName = _baseInstanceName,
+  ]) {
     _dependencies[_InjectorKey(instanceName, T)] = factory;
   }
 
   /// get the dependency instance of the type [T] with instance name of [instanceName]
-  T get<T>([String instanceName = _baseInstanceName]) {
+  T get<T>([
+    String instanceName = _baseInstanceName,
+  ]) {
     final dep = _dependencies[_InjectorKey(instanceName, T)];
     if (dep is Function) {
       final value = dep();
