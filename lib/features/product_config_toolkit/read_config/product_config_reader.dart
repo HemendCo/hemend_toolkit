7a8eb84df58fc198d7a5d0fed938c8c484591ab1
FMotalleb <fmotalleb@gmail.com>
2022-06-21 14:51:26 +0430

    [HemSpec] boosted parser so it can pars queries supports simple WHERE queries support link queries formated

--- lib/features/product_config_toolkit/read_config/product_config_reader.dart
+++ lib/features/product_config_toolkit/read_config/product_config_reader.dart
@@ -8,6 +8,53 @@ import 'package:yaml/yaml.dart' show loadYaml, YamlMap;
 import '../../build_tools/core/contracts/typedefs/typedefs.dart';
 import '../core/product_config_defaults.dart' show kProductConfigFileName;
 
+EnvironmentParams readConfigLinks() {
+  if (!ProjectConfigs.hasHemendspec) {
+    cli.printToConsole(
+      'Hemendspec file not found.',
+      isError: true,
+    );
+
+    cli.printToConsole('you can generate it with `hem init`.');
+    exit(64);
+  }
+
+  try {
+    final config = loadYaml(File(kProductConfigFileName).readAsStringSync()) as YamlMap;
+    final params = _applyRules(
+      _castToEnvParams(
+        dissolveNestedItems(
+          config['ENV_CONFIG'],
+          'ENV_CONFIG',
+        ),
+        {},
+      ),
+      deInjector.get<Map<String, String>>(),
+    )..addAll(deInjector.get<Map<String, String>>());
+    deInjector.get<Map<String, String>>().addAll(params);
+    cli.verbosePrint('Environments config: $params');
+    return params;
+  } catch (e) {
+    cli.printToConsole(
+      'cannot read config file.',
+      isError: true,
+    );
+    cli.printToConsole(
+      '''regenerate it with `hem init --force`.
+otherwise you can fix this issue by editing the file manually.
+the issue is in 'hemendspec.yaml'
+ENV_CONFIG:
+    *: *
+without any `-` before the key
+
+the exception is $e
+''',
+      isError: true,
+    );
+    exit(64);
+  }
+}
+
 EnvironmentParams readHemendCliConfig() {
   if (!ProjectConfigs.hasHemendspec) {
     cli.printToConsole(
@@ -25,6 +72,7 @@ EnvironmentParams readHemendCliConfig() {
         config['HEMEND_CONFIG'],
         'HEMEND_CONFIG',
       ),
+      readConfigLinks(),
     );
     deInjector.get<Map<String, String>>().addAll(params);
     cli.verbosePrint('Hem config: $params');
@@ -38,9 +86,8 @@ EnvironmentParams readHemendCliConfig() {
       '''regenerate it with `hem init --force`.
 otherwise you can fix this issue by editing the file manually.
 the issue is in 'hemendspec.yaml'
-env:
-  CONFIG:
-    *: *
+HEMEND_CONFIG:
+  *: *
 without any `-` before the key
 
 the exception is $e
@@ -60,6 +107,7 @@ EnvironmentParams readProductConfig() {
     cli.printToConsole('you can generate it with `hem init`.');
     exit(64);
   }
+
   try {
     final config = loadYaml(File(kProductConfigFileName).readAsStringSync()) as YamlMap;
     final params = _castToEnvParams(
@@ -67,6 +115,7 @@ EnvironmentParams readProductConfig() {
         config['ENV'],
         'CONFIG',
       ),
+      readConfigLinks(),
     );
     deInjector.get<Map<String, String>>().addAll(params);
     cli.verbosePrint('app config: $params');
@@ -80,9 +129,8 @@ EnvironmentParams readProductConfig() {
       '''regenerate it with `hem init --force`.
 otherwise you can fix this issue by editing the file manually.
 the issue is in 'hemendspec.yaml'
-env:
-  CONFIG:
-    *: *
+ENV:
+  *: *
 without any `-` before the key
 
 the exception is $e
@@ -93,8 +141,94 @@ the exception is $e
   }
 }
 
-EnvironmentParams _castToEnvParams(Map<dynamic, dynamic> from) {
-  return Map<String, String>.fromEntries(from.entries.map((e) => MapEntry(e.key.toString(), e.value.toString())));
+EnvironmentParams _castToEnvParams(Map<dynamic, dynamic> from, EnvironmentParams links) {
+  return Map<String, String>.fromEntries(
+      from.entries.map((e) => MapEntry(e.key.toString(), _applyLink(e.value.toString(), links))));
+}
+
+Map<String, String> normalizerSheetMap = {
+  r'$empStr': '',
+  r'$spaceStr': ' ',
+};
+String _normalizeArgs(String arg) {
+  return normalizerSheetMap[arg] ?? arg;
+}
+
+EnvironmentParams _applyRules(EnvironmentParams base, EnvironmentParams rules) {
+  cli.verbosePrint('internal environments config: $rules');
+
+  final result = EnvironmentParams.from(base);
+  for (final i in rules.entries) {
+    for (final item in base.entries) {
+      final args = item.value.split(' ');
+      try {
+        if (args[0] == 'WHERE' && args[1] == i.key) {
+          switch (args[2]) {
+            case '=':
+              if (args[3] == i.value) {
+                result[item.key] = _normalizeArgs(args[5]);
+              } else {
+                result[item.key] = _normalizeArgs(args[7]);
+              }
+              break;
+            case '<=':
+              final intValue = int.parse(args[3]);
+              if (intValue >= int.parse(i.value)) {
+                result[item.key] = _normalizeArgs(args[5]);
+              } else {
+                result[item.key] = _normalizeArgs(args[7]);
+              }
+              break;
+            case '>=':
+              final intValue = int.parse(args[3]);
+              if (intValue <= int.parse(i.value)) {
+                result[item.key] = _normalizeArgs(args[5]);
+              } else {
+                result[item.key] = _normalizeArgs(args[7]);
+              }
+              break;
+            case '>':
+              final intValue = int.parse(args[3]);
+              if (intValue < int.parse(i.value)) {
+                result[item.key] = _normalizeArgs(args[5]);
+              } else {
+                result[item.key] = _normalizeArgs(args[7]);
+              }
+              break;
+            case '<':
+              final intValue = int.parse(args[3]);
+              if (intValue > int.parse(i.value)) {
+                result[item.key] = _normalizeArgs(args[5]);
+              } else {
+                result[item.key] = _normalizeArgs(args[7]);
+              }
+              break;
+            default:
+              throw Exception('this Query has not implemented yet');
+          }
+        }
+      } catch (e) {
+        cli.printToConsole(
+          'cannot apply a rule cause: $e',
+          isError: true,
+        );
+      }
+    }
+  }
+  cli.verbosePrint('''
+  base config: $base
+
+  after applying rules: $result
+''');
+  return result;
+}
+
+String _applyLink(String input, EnvironmentParams links) {
+  String result = input;
+  for (final key in links.keys) {
+    result = result.replaceAll('\${$key}', links[key] ?? '');
+  }
+  return result;
 }
 
 Map<String, dynamic> dissolveNestedItems(Map<dynamic, dynamic> params, [String? prefix]) {
--- lib/features/product_config_toolkit/sample_creator/product_config_sample_creator.dart
+++ lib/features/product_config_toolkit/sample_creator/product_config_sample_creator.dart
@@ -7,42 +7,76 @@ import 'package:json2yaml/json2yaml.dart';
 import '../core/product_config_defaults.dart';
 
 abstract class ProductConfigSampleCreator {
-  static const _comments = '''
+  static const _comments = r'''
+#
+# ──────────────────────────────────────────────────────────────────────────
+#   :::::: E N V I R O N E M T N S : :  :   :    :     :        :          :
+# ──────────────────────────────────────────────────────────────────────────
+#
+# for more information about about environment variables use: `hem env`
 # add app config here (if any)
 # values here can be accessed in the app from dart environment variable
 # e.g.: String.fromEnvironment() and etc
 # the only supported format is Map<String,String>
 # supports nested maps but at the end all of them will be used as Map<String,String>
 # e.g.:
-# env:
+# ENV_CONFIG:
+# API:
+#   VERSION: "1"
+#   SUFFIX: "WHERE DEBUG_LEVEL >= 1 ? /demo : $empStr"
+#
+# HEMEND_CONFIG:
+#   NAME_FORMAT: "$n%-$v%-$build_type%-$YYYY%\\$MM%\\$DD%-$HH%:$mm%:$ss%"
+#   CLI_VERSION: 0.1
+# ENV:
 #   CRASHLYTIX:
 #     APP:
 #       SECRET: Add Crashlytix App Secret Here
 #       ID: Add Crashlytix App ID Here
 #     SERVER:
-#       ADDRESS: Add Crashlytix Server Address Here
+#       ADDRESS: "example.com/api/v${ENV_CONFIG_API_VERSION}/crashlytix this will translate to example.com/api/v1/crashlytix"
+
+# ENV_CONFIG is usable variables inside the hemspec config file
+# in this section you can set variables which are static or they can have queries
+# to get values from `hem cli` internal environment configs
+# query reader uses `split(' ')` to split the query so you have to use spaces in the query to split its section
+# if you want to set some thing to empty string use `$empStr` it will be replaced with empty string (`` not ` ` it don't set space)
+# if you want to see what environment variables are accessible in this phase you can run `hem env`
+
 # config parser will add 'CONFIG' prefix to each key to prevent collision with core configs
 # e.g. ("BUILD_TIME", "LAST_GIT_COMMIT", "DEBUG_LEVEL", etc.)
 # config parser will concat the nested keys to their root with '_' as separator
 # in the yaml code snippet above, the key of app secret will be: "CONFIG_CRASHLYTIX_APP_SECRET"
 
 # DO NOT remove the default configs they are used by hemend core package
-
 ''';
 
   static Future<Map<String, dynamic>> get _sampleAppConfig async => {
+        'ENV_CONFIG': {
+          'API': {
+            'VERSION': r'1',
+            'SUFFIX': r'WHERE DEBUG_LEVEL >= 1 ? /demo : $empStr',
+          },
+        },
         'HEMEND_CONFIG': {
           'NAME_FORMAT': r'$n%-$v%-$build_type%-$YYYY%\\$MM%\\$DD%-$HH%:$mm%:$ss%',
           'CLI_VERSION': InternalStaticInfo.CLI_VERSION,
         },
         'ENV': {
+          'APP': {
+            'API': {
+              'BASE': r'example.com${ENV_CONFIG_API_SUFFIX}',
+              'VERSION': r'${ENV_CONFIG_API_VERSION}',
+            },
+          },
           'CRASHLYTIX': {
             'APP': {
               'SECRET': 'Add Crashlytix App Secret Here',
               'ID': 'Add Crashlytix App ID Here',
             },
             'SERVER': {
-              'ADDRESS': 'Add Crashlytix Server Address Here',
+              'ADDRESS':
+                  r'example.com/api/v${ENV_CONFIG_API_VERSION}/crashlytix this will translate to example.com/api/v1/crashlytix',
             },
           }
         }
