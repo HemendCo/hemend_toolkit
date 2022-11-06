5ac0abcaa007460659f51760b34c797f34ed1f1b
FMotalleb <fmotalleb@gmail.com>
2022-11-06 10:26:12 +0330

    [Fix] build Issue

--- lib/core/hemend_toolkit_config/app_config_parser/app_config_parser.dart
+++ lib/core/hemend_toolkit_config/app_config_parser/app_config_parser.dart
@@ -307,7 +307,7 @@ ${IHemendCustomConfigModel.helpGenerator}
         showHelp('invalid environment variable: $env the format is: key=value');
       }
     }
-    return extraArgs;
+    return extraArgs.map((key, value) => MapEntry(key.replaceAll(' ', '_'), value.replaceAll(' ', '_')));
   }
 
   // static Map<String, dynamic> _dissolveEnvType(Map<String, String> env) {
