1e63eb3e6152d8db91210ecfcae4ed55d37d9d31
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2022-11-10 09:33:49 +0330

    [Fix]

--- lib/core/io/command_line_toolkit/command_line_tools.dart
+++ lib/core/io/command_line_toolkit/command_line_tools.dart
@@ -11,8 +11,8 @@ final HemTerminal cli = HemTerminal._();
 class HemTerminal {
   Logger _logger = Logger.standard();
   Duration? get elapsedTime {
-    if (_logger is VerboseLogger) {
-      return (_logger as VerboseLogger).timer;
+    if (_logger is VerboseLoggerCustom) {
+      return (_logger as VerboseLoggerCustom).timer;
     } else {
       return null;
     }
@@ -20,7 +20,7 @@ class HemTerminal {
 
   HemTerminal._();
   void useVerbosLogger() {
-    _logger = Logger.verbose();
+    _logger = VerboseLoggerCustom();
     printToConsole('using verbose logger config');
   }
 
@@ -148,4 +148,75 @@ class _ProcessParams {
   final List<String> args;
 
   _ProcessParams(this.exe, this.args);
+}
+
+class VerboseLoggerCustom implements Logger {
+  @override
+  Ansi ansi;
+  bool logTime;
+  final _timer = Stopwatch()..start();
+  Duration get timer => _timer.elapsed;
+  VerboseLoggerCustom({
+    Ansi? ansi,
+    this.logTime = false,
+  }) : ansi = ansi ??
+            Ansi(
+              Ansi.terminalSupportsAnsi,
+            );
+
+  @override
+  bool get isVerbose => true;
+
+  @override
+  void stdout(String message) {
+    io.stdout.writeln('${_createPrefix()}$message');
+  }
+
+  @override
+  void stderr(String message) {
+    io.stderr.writeln('${_createPrefix()}${ansi.red}$message${ansi.none}');
+  }
+
+  @override
+  void trace(String message) {
+    io.stdout.writeln('${_createPrefix()}${ansi.gray}$message${ansi.none}');
+  }
+
+  @override
+  void write(String message) {
+    io.stdout.write(message);
+  }
+
+  @override
+  void writeCharCode(int charCode) {
+    io.stdout.writeCharCode(charCode);
+  }
+
+  @override
+  Progress progress(String message) => SimpleProgress(this, message);
+
+  @override
+  @Deprecated('This method will be removed in the future')
+  void flush() {}
+
+  String _createPrefix() {
+    if (!logTime) {
+      return '';
+    }
+
+    var seconds = _timer.elapsedMilliseconds / 1000.0;
+    var minutes = seconds ~/ 60;
+    seconds -= minutes * 60.0;
+
+    var buf = StringBuffer();
+    if (minutes > 0) {
+      buf.write((minutes % 60));
+      buf.write('m ');
+    }
+
+    buf.write(seconds.toStringAsFixed(3).padLeft(minutes > 0 ? 6 : 1, '0'));
+    buf.write('s');
+
+    return '[${buf.toString().padLeft(11)}] ';
+  }
 }
