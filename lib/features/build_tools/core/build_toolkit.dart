db6ea3d05fd88c94f8084fa3e1d9b514011c24d3
FMotalleb <fmotalleb@gmail.com>
2022-11-14 11:51:12 +0330

    [Feat] speed meter and ETA for upload

--- lib/features/build_tools/core/build_toolkit.dart
+++ lib/features/build_tools/core/build_toolkit.dart
@@ -79,20 +79,27 @@ abstract class BuildToolkit {
             final apiBase = env['HEMEND_CONFIG_UPLOAD_API'];
             final apiPath = env['HEMEND_CONFIG_UPLOAD_PATH'];
             final url = '$apiBase$apiPath';
+            final timer = Stopwatch();
             final request = http.MultipartRequestProgress(
               'POST',
               Uri.parse(url),
               onProgress: (bytes, totalBytes) {
+                if (timer.isRunning != true) {
+                  timer.start();
+                }
                 final time = cli.elapsedTime;
                 var prefix = '';
                 if (time != null) {
                   prefix = '${formatter(time)} ';
                 }
-                final sizeText =
-                    '${(bytes / 1000000).toStringAsFixed(1)}/${(totalBytes / 1000000).toStringAsFixed(1)}mb';
+                final sentMb = bytes / 1000000;
+                final sizeText = '${sentMb.toStringAsFixed(1)}/${(totalBytes / 1000000).toStringAsFixed(1)}Mb';
                 final percentage = '''${((bytes / totalBytes) * 100).toStringAsFixed(2)}%''';
+                final passedTime = timer.elapsed.inMilliseconds / 1000;
+                final speedApprox = (sentMb / passedTime).toStringAsFixed(2);
+                final eta = (((totalBytes / 1000000) - sentMb) / double.parse(speedApprox)).toStringAsFixed(3);
                 stdout.write(
-                  '''\x1B[2K\r${prefix}Upload Progress: $percentage $sizeText''',
+                  '''\x1B[2K\r${prefix}Upload Progress: $percentage $sizeText ~${speedApprox}Mbps ~${eta}s left''',
                 );
               },
             );
@@ -107,6 +114,7 @@ abstract class BuildToolkit {
             final response = await request.send();
 
             final responseData = await response.stream.toBytes();
+            timer.stop();
             stdout.write('\x1B[2K\r');
             final responseString = String.fromCharCodes(responseData);
             final responseJson = RegExp('\\[.*]') //
